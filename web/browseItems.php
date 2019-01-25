156f2b6dbe07d59f8f1ad3dbd6ed830406481948
Zach <bru15012@byui.edu>
2019-01-25 15:37:02 -0700

    First Shopping

---
+++ web/browseItems.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Zach's Homepage</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="homepage.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="homepage.php">Zach's Homepage</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="homepage.php">Home</a></li>
+                <li><a href="individualAssignmentIndex.html">Individual Assignments</a></li>
+                <li><a href="teamActivityIndex.html">Group Assignments</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </div>
+        </nav><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+            </div>
+            <div class="row">
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+            </div>
+            <div class="row">
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+                <div class="c0l-sm-4"></div>
+            </div>
+        </div>
+    </body>
+</html>

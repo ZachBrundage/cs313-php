1c4338e2bb7f5dc7f72695820367bb3353841df9
Zach <bru15012@byui.edu>
2019-01-30 00:04:53 -0700

    session 01

--- web/cart.php
+++ web/cart.php
@@ -1,20 +1,23 @@
 <?php
     session_start();
-    $OMAM = $_POST['OMAM'];
-    $brandNew = $_POST['brandNew'];
-    $MICRO = $_POST['micro'];
-    $modBase = $_POST['modBase'];
-    $silPlanet = $_POST['silPlanet'];
-    $DGD = $_POST['DGD'];
-    $mooseBlood = $_POST['mooseBlood'];
-    $ADTR = $_POST['ADTR'];
-    $wonderYears = $_POST['wonderYears'];
+    $_SESSEION["OMAM"] = $_POST['OMAM'];
+    $_SESSEION["brandNew"] = $_POST['brandNew'];
+    $_SESSEION["micro"] = $_POST['micro'];
+    $_SESSEION["modBase"] = $_POST['modBase'];
+    $_SESSEION["silPlanet"] = $_POST['silPlanet'];
+    $_SESSEION["DGD"] = $_POST['DGD'];
+    $_SESSEION["mooseBlood"] = $_POST['mooseBlood'];
+    $_SESSEION["ADTR"] = $_POST['ADTR'];
+    $_SESSEION["wonderYears"] = $_POST['wonderYears'];
 ?>
 
 <!DOCTYPE html>
 <html>
     <head></head>
     <body>
+        
+        <?php print_r($_SESSION); ?>
+        
         <h1>OMAM: <?php echo $OMAM; ?></h1>
         <h1>Brand New: <?php echo $brandNew; ?></h1>
         <h1>Microwave: <?php echo $MICRO; ?></h1>

1ed428062b610e225e19a2e45a3825393fecaf34
Zach Brundage <bru15012@byui.edu>
2019-02-06 14:34:11 -0700

    nav bar

--- web/prj01LogIn.php
+++ web/prj01LogIn.php
@@ -6,19 +6,6 @@
         <script type="text/javascript" src="prj01.js"></script>
     </head>
     <body>
-        <nav class="navbar navbar-default">
-        <div class="container-fluid">
-            <div class="navbar-header">
-                <a class="navbar-brand" href="homepage.php">Zach's Homepage</a>
-            </div>
-            <ul class="nav navbar-nav">
-                <li><a href="homepage.php">Home</a></li>
-                <li><a href="individualAssignmentIndex.html">Individual Assignments</a></li>
-                <li class="active"><a href="teamActivityIndex.html">Group Assignments</a></li>
-                <li><a href="contact.html">Contact</a></li>
-            </ul>
-        </div>
-        </nav><br><br>
         <div class="container">
             <form action="" method="post">
                 <h1>Sign In</h1>

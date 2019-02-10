8c442c6859dcfd26223b2590d88ff9269c617222
Zach Brundage <bru15012@byui.edu>
2019-02-09 20:18:04 -0700

    UPDATE

--- web/prj01Dash.php
+++ web/prj01Dash.php
@@ -32,7 +32,7 @@ $userid = $marker['userid'];
 <!DOCTYPE html>
 <html>
     <head>
-        <link src="css/bootstrap-theme.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.3/Chart.js"></script>
         <script src="prj01.js" type="text/javascript"></script>
     </head>

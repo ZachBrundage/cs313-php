e1a3263692f4262d9dc7f57907e09ce1997ff603
Zach Brundage <bru15012@byui.edu>
2019-02-21 17:50:59 -0700

    UPDATE

--- web/prj01/verifyLogin.php
+++ web/prj01/verifyLogin.php
@@ -6,13 +6,16 @@ $db = get_db();
 $username = $_POST["Username"];
 $password = $_POST["Password"];
 
+echo $username;
+echo $password;
+/*
 $query = 'SELECT userid FROM users WHERE username = $username AND userpass = $password';
 $stmt = $db->prepare($query);
 $stmt->execute();
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 var_dump($results);
-
+*/
 //$_SESSION["userid"] = $results['userid'];
 ?>
 <!DOCTYPE html>

7cb0818e44ee1325d90153d4b83287248a3318ca
Zach Brundage <bru15012@byui.edu>
2019-02-23 22:00:55 -0700

    UPDATE

--- web/prj01/dash.php
+++ web/prj01/dash.php
@@ -15,38 +15,45 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
         <link href="style.css" rel="stylesheet">
     </head>
     <body>
-        <h1> Dashboard </h1>
-            <table>
-                <tr>
-                    <th> Entry Date</th>
-                    <th> Weight</th>
-                    <th> Workout Type</th>
-                    <th> Caloric Intake</th>
-                    <th> Calories Burned</th>
-                </tr>
-                <?php
-                foreach ($results as $result)
-                {
-                    $entryDate = $result['entrydate'];
-                    $weight = $result['weight'];
-                    $workout = $result['workouttype'];
-                    $caloricIntake = $result['caloricintake'];
-                    $caloriesBurned = $result['caloriesburned'];
+        <div class="container-dash">
+            <div class="header-login">
+                <h1 class="headerText"> Dashboard </h1>
+            </div>
+            <p class="p-login">Your Entries</p>
+                <table>
+                    <tr>
+                        <th> Entry Date</th>
+                        <th> Weight</th>
+                        <th> Workout Type</th>
+                        <th> Caloric Intake</th>
+                        <th> Calories Burned</th>
+                    </tr>
+                    <?php
+                    foreach ($results as $result)
+                    {
+                        $entryDate = $result['entrydate'];
+                        $weight = $result['weight'];
+                        $workout = $result['workouttype'];
+                        $caloricIntake = $result['caloricintake'];
+                        $caloriesBurned = $result['caloriesburned'];
 
-                    echo "<tr>";
-                    echo "<td>$entryDate</td>";
-                    echo "<td>$weight</td>";
-                    echo "<td>$workout</td>";
-                    echo "<td>$caloricIntake</td>";
-                    echo "<td>$caloriesBurned</td>";
-                    echo "</tr>";
-                }
-                ?>
-            </table>
-        <form action="results.php" method="post">
-            <input type="text" name="search">
-            <input type="submit" value="Search">
-        </form>
-        <a href="newEntry.php"><button>Create New Entry</button></a>
+                        echo "<tr>";
+                        echo "<td>$entryDate</td>";
+                        echo "<td>$weight</td>";
+                        echo "<td>$workout</td>";
+                        echo "<td>$caloricIntake</td>";
+                        echo "<td>$caloriesBurned</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+                </table><br><br>
+            <p class="p-login">Search Records By Date</p>
+            <form action="results.php" method="post">
+                <input type="text" name="search" class="input-dash" value="YYYY-MM-DD"><br><br>
+                <input type="submit" value="Search" class="button-dash">
+            </form>
+            <p class="p-login">Or</p>
+            <a href="newEntry.php" ><button class="button-dash">Create New Entry</button></a>
+        </div>
     </body>
 </html>

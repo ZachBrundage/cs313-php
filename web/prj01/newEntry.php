87cc14649f7d604d3a13b1d8b832672f58cd9aa6
Zach Brundage <bru15012@byui.edu>
2019-02-23 22:09:46 -0700

    UPDATE

--- web/prj01/newEntry.php
+++ web/prj01/newEntry.php
@@ -1,16 +1,21 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <link href="style.css" rel="stylesheet">
     </head>
     <body>
-        <h1>Create New Entry</h1>
-        <form action="insertEntry.php" method="post">
-            <input type="text" value="Entry Date" name="entryDate">
-            <input type="text" value="weight" name="weight">
-            <input type="text" value="workout" name="workout">
-            <input type="text" value="caloricIntake" name="caloricIntake">
-            <input type="text" value="caloriesBurned" name="caloriesBurned">
-            <input type="submit" value="Submit">
-        </form>
+        <div class="container-dash">
+            <div class="header-login">
+                <h1 class="headerText">Create New Entry</h1>
+            </div>
+            <form action="insertEntry.php" method="post">
+                <input type="text" value="Entry Date" name="entryDate" class="input-dash"><br><br>
+                <input type="text" value="weight" name="weight" class="input-dash"><br><br>
+                <input type="text" value="workout" name="workout" class="input-dash"><br><br>
+                <input type="text" value="caloricIntake" name="caloricIntake" class="input-dash"><br><br>
+                <input type="text" value="caloriesBurned" name="caloriesBurned" class="input-dash"><br><br>
+                <input type="submit" value="Submit" class="button-dash">
+            </form>
+        </div>
     </body>
 </html>
